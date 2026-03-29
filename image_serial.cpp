83488b6f52098dbaccbc5c3b3a93d51c665764ab
Ayman Issa <ayman.issa2023new@gmail.com>
2026-03-29 22:19:38 +0300

    Initial project setup

---
+++ image_serial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+struct Pixel {
+    unsigned char r, g, b;
+};
+
+int main() {
+    cout << "Image Processing Project - Serial Version" << endl;
+    return 0;
+}
